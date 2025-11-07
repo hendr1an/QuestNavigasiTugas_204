0090a398bb501f6e4cca8c1ede12c3ae9f53dc5c
hendr1an <hendriansyahgyan@gmail.com>
2025-11-07 20:41:12 +0700

    mengkonfigurasi ulang main activity

--- app/src/main/java/com/example/questnavigasitugas_204/MainActivity.kt
+++ app/src/main/java/com/example/questnavigasitugas_204/MainActivity.kt
@@ -5,43 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.questnavigasitugas_204.ui.theme.QuestNavigasiTugas_204Theme
+import com.example.questnavigasitugas_204.ui.theme.questnavigasitugas_204Theme // Sesuaikan nama tema Anda
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            QuestNavigasiTugas_204Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            questnavigasitugas_204Theme { // Menggunakan tema aplikasi Anda
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    NavApp() // Panggil komponen NavApp kita
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestNavigasiTugas_204Theme {
-        Greeting("Android")
     }
 }
